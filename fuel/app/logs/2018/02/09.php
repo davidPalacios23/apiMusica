376816892d23b06c2f18e575ab3b73e987a366f6
davidPalacios23 <davidpalacios23@hotmail.com>
2018-02-13 08:32:45 -0500

    commit de prueba

---
+++ fuel/app/logs/2018/02/09.php
@@ -0,0 +1,5 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2018-02-09 13:02:12 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-09 13:07:25 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-09 13:17:09 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
