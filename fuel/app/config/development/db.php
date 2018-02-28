6a83220bbd35fa6a5e2d433c565806ea006e0358
davidPalacios23 <davidpalacios23@hotmail.com>
2018-02-28 05:31:35 -0500

    user, pass y nombre de bbdd modificados para servidor de produccion

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -6,9 +6,9 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=appMusic',
-			'username'   => 'alumnos',
-			'password'   => 'cev',
+			'dsn'        => 'mysql:host=localhost;dbname=david',
+			'username'   => 'david',
+			'password'   => 'bitterblossom',
 		),
 	),
 );
